06d3bebebe8fc85a78c2675b4034ad9966ab3372
dracat <dracat@gmail.com>
2018-03-20 11:15:58 +0300

    f

--- js/script3.js
+++ js/script3.js
@@ -88,7 +88,7 @@ var openPhotoSwipe = function( name) {
      $.each( data.resources, function( i, it ) {
 
      items.push( {
-	    src: 'https://res.cloudinary.com/moscowknot/image/upload/w_1000,h_1000,c_limit/' + it.public_id + '.jpg',
+	    src: 'https://res.cloudinary.com/moscowknot/image/upload/v'+it.version+'/w_1000,h_1000,c_limit/' + it.public_id + '.jpg',
         w: it.width,
         h: it.height
 	  });
